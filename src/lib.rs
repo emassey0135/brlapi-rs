64b32bc7ed521ee089d458af380684bfb688815e
Elijah Massey <emassey0135@gmail.com>
2025-06-01 14:46:33 -0500

    Add foundations for packet types

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use binrw::binrw;
+#[binrw]
+#[brw(big)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum PacketType {
+  #[brw(magic(b"\0\0\0v"))]
+  Version,
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[binrw]
+#[brw(big, import(size: u32, ty: PacketType))]
+#[derive(Debug, PartialEq, Eq, Clone)]
+enum PacketData {
+  #[br(pre_assert(ty == PacketType::Version), pre_assert(size == 1))]
+  Version(u32),
+}
+#[binrw]
+#[brw(big)]
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct Packet {
+  size: u32,
+  ty: PacketType,
+  #[br(args(size, ty))]
+  #[bw(args(*size, *ty))]
+  data: PacketData,
 }
