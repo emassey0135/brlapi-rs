1b41d101c6c502368614ccffee265a641de42488
Elijah Massey <emassey0135@gmail.com>
2025-06-01 22:51:40 -0500

    Add auth packets

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,44 @@
-use binrw::binrw;
+use binrw::{NullString, binrw};
+#[binrw]
+#[brw(big)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum AuthType {
+  #[brw(magic(b"\0\0\0N"))]
+  None,
+  #[brw(magic(b"\0\0\0K"))]
+  Key,
+  #[brw(magic(b"\0\0\0C"))]
+  Credentials,
+}
 #[binrw]
 #[brw(big)]
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum PacketType {
   #[brw(magic(b"\0\0\0v"))]
   Version,
+  #[brw(magic(b"\0\0\0a"))]
+  Auth,
 }
 #[binrw]
 #[brw(big, import(size: u32, ty: PacketType))]
 #[derive(Debug, PartialEq, Eq, Clone)]
 enum PacketData {
-  #[br(pre_assert(ty == PacketType::Version), pre_assert(size == 1))]
-  Version(u32),
+  #[br(pre_assert(ty == PacketType::Version))]
+  #[brw(assert(size == 4))]
+  Version { version: u32 },
+  #[br(pre_assert(ty == PacketType::Auth))]
+  #[br(assert(size as usize == auth_types.len()*4))]
+  AuthRequest {
+    #[br(count(size/4))]
+    auth_types: Vec<AuthType>,
+  },
+  #[br(pre_assert(ty == PacketType::Auth))]
+  #[br(assert(auth_type == AuthType::Key))]
+  #[br(assert(size as usize == key.len()+4))]
+  AuthResponse {
+    auth_type: AuthType,
+    key: NullString,
+  },
 }
 #[binrw]
 #[brw(big)]
