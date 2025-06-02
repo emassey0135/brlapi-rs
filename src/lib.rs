5e413347f0ec083ee16299cc094c6fa2f0252e3b
Elijah Massey <emassey0135@gmail.com>
2025-06-01 23:06:35 -0500

    Add more packets and fix some directives

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,12 @@ enum PacketType {
   Version,
   #[brw(magic(b"\0\0\0a"))]
   Auth,
+  #[brw(magic(b"\0\0\0n"))]
+  GetDriverName,
+  #[brw(magic(b"\0\0\0d"))]
+  GetModelId,
+  #[brw(magic(b"\0\0\0s"))]
+  GetDisplaySize,
 }
 #[binrw]
 #[brw(big, import(size: u32, ty: PacketType))]
@@ -27,18 +33,37 @@ enum PacketData {
   #[brw(assert(size == 4))]
   Version { version: u32 },
   #[br(pre_assert(ty == PacketType::Auth))]
-  #[br(assert(size as usize == auth_types.len()*4))]
+  #[brw(assert(size as usize == auth_types.len()*4))]
   AuthRequest {
     #[br(count(size/4))]
     auth_types: Vec<AuthType>,
   },
   #[br(pre_assert(ty == PacketType::Auth))]
   #[br(assert(auth_type == AuthType::Key))]
-  #[br(assert(size as usize == key.len()+4))]
+  #[bw(assert(*auth_type == AuthType::Key))]
+  #[brw(assert(size as usize == key.len()+4))]
   AuthResponse {
     auth_type: AuthType,
     key: NullString,
   },
+  #[br(pre_assert(ty == PacketType::GetDriverName))]
+  #[br(pre_assert(size == 0))]
+  GetDriverNameRequest,
+  #[br(pre_assert(ty == PacketType::GetDriverName))]
+  #[brw(assert(size as usize == driver.len()))]
+  GetDriverNameResponse { driver: NullString },
+  #[br(pre_assert(ty == PacketType::GetModelId))]
+  #[br(pre_assert(size == 0))]
+  GetModelIdRequest,
+  #[br(pre_assert(ty == PacketType::GetModelId))]
+  #[brw(assert(size as usize == model.len()))]
+  GetModelIdResponse { model: NullString },
+  #[br(pre_assert(ty == PacketType::GetDisplaySize))]
+  #[br(pre_assert(size == 0))]
+  GetDisplaySizeRequest,
+  #[br(pre_assert(ty == PacketType::GetDisplaySize))]
+  #[brw(assert(size == 8))]
+  GetDisplaySizeResponse { width: u32, height: u32 },
 }
 #[binrw]
 #[brw(big)]
