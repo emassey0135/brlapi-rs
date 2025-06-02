442d97849fcf78fa0f1b85254976480eae1b92e7
Elijah Massey <emassey0135@gmail.com>
2025-06-02 12:16:27 -0500

    Calculate size field from size of packet data and calculate some other length fields, and assert them when reading

--- src/lib.rs
+++ src/lib.rs
@@ -38,26 +38,27 @@ enum PacketType {
   LeaveTtyMode,
 }
 #[binrw]
-#[brw(big, import(size: u32, ty: PacketType))]
+#[brw(big)]
+#[br(import(size: u32, ty: PacketType))]
 #[derive(Debug, PartialEq, Eq, Clone)]
 enum PacketData {
   #[br(pre_assert(ty == PacketType::Ack))]
   #[br(pre_assert(size == 0))]
   AckResponse,
   #[br(pre_assert(ty == PacketType::Error))]
-  #[brw(assert(size == 4))]
+  #[br(assert(size == 4))]
   ErrorResponse { error: u32 },
   #[br(pre_assert(ty == PacketType::Exception))]
-  #[brw(assert(size as usize == packet.len()))]
+  #[br(assert(size as usize == packet.len()))]
   ExceptionResponse {
     #[br(count(size))]
     packet: Vec<u8>,
   },
   #[br(pre_assert(ty == PacketType::Version))]
-  #[brw(assert(size == 4))]
+  #[br(assert(size == 4))]
   Version { version: u32 },
   #[br(pre_assert(ty == PacketType::Auth))]
-  #[brw(assert(size as usize == auth_types.len()*4))]
+  #[br(assert(size as usize == auth_types.len()*4))]
   AuthRequest {
     #[br(count(size/4))]
     auth_types: Vec<AuthType>,
@@ -65,7 +66,7 @@ enum PacketData {
   #[br(pre_assert(ty == PacketType::Auth))]
   #[br(assert(auth_type == AuthType::Key))]
   #[bw(assert(*auth_type == AuthType::Key))]
-  #[brw(assert(size as usize == key.len()+4))]
+  #[br(assert(size as usize == key.len()+4))]
   AuthResponse {
     auth_type: AuthType,
     key: NullString,
@@ -74,48 +75,76 @@ enum PacketData {
   #[br(pre_assert(size == 0))]
   GetDriverNameRequest,
   #[br(pre_assert(ty == PacketType::GetDriverName))]
-  #[brw(assert(size as usize == driver.len()))]
+  #[br(assert(size as usize == driver.len()))]
   GetDriverNameResponse { driver: NullString },
   #[br(pre_assert(ty == PacketType::GetModelId))]
   #[br(pre_assert(size == 0))]
   GetModelIdRequest,
   #[br(pre_assert(ty == PacketType::GetModelId))]
-  #[brw(assert(size as usize == model.len()))]
+  #[br(assert(size as usize == model.len()))]
   GetModelIdResponse { model: NullString },
   #[br(pre_assert(ty == PacketType::GetDisplaySize))]
   #[br(pre_assert(size == 0))]
   GetDisplaySizeRequest,
   #[br(pre_assert(ty == PacketType::GetDisplaySize))]
-  #[brw(assert(size == 8))]
+  #[br(assert(size == 8))]
   GetDisplaySizeResponse { width: u32, height: u32 },
   #[br(pre_assert(ty == PacketType::EnterTtyMode))]
-  #[brw(assert(size == 8+ttys_len+driver_len))]
+  #[br(assert(size as usize == 8+ttys.len()+driver.len()))]
   #[br(assert(ttys_len as usize == ttys.len()))]
-  #[bw(assert(*ttys_len as usize == ttys.len()))]
   #[br(assert(driver_len as usize == driver.len()))]
-  #[bw(assert(*driver_len as usize == driver.len()))]
   EnterTtyModeRequest {
+    #[br(temp)]
+    #[bw(calc(ttys.len() as u32))]
     ttys_len: u32,
     #[br(count(ttys_len))]
     ttys: Vec<u32>,
+    #[br(temp)]
+    #[bw(calc(driver.len() as u32))]
     driver_len: u32,
     #[br(count(driver_len))]
     driver: Vec<u8>,
   },
   #[br(pre_assert(ty == PacketType::SetFocus))]
-  #[brw(assert(size == 4))]
+  #[br(assert(size == 4))]
   SetFocusRequest { tty: u32 },
   #[br(pre_assert(ty == PacketType::LeaveTtyMode))]
   #[br(pre_assert(size == 0))]
   LeaveTtyModeRequest,
 }
+impl PacketData {
+  fn size(&self) -> usize {
+    match self {
+      PacketData::AckResponse => 0,
+      PacketData::ErrorResponse { error: _ } => 4,
+      PacketData::ExceptionResponse { packet } => packet.len(),
+      PacketData::Version { version: _ } => 4,
+      PacketData::AuthRequest { auth_types } => auth_types.len() / 4,
+      PacketData::AuthResponse { auth_type: _, key } => key.len() + 4,
+      PacketData::GetDriverNameRequest => 0,
+      PacketData::GetDriverNameResponse { driver } => driver.len(),
+      PacketData::GetModelIdRequest => 0,
+      PacketData::GetModelIdResponse { model } => model.len(),
+      PacketData::GetDisplaySizeRequest => 0,
+      PacketData::GetDisplaySizeResponse {
+        width: _,
+        height: _,
+      } => 8,
+      PacketData::EnterTtyModeRequest { ttys, driver } => 8 + ttys.len() + driver.len(),
+      PacketData::SetFocusRequest { tty: _ } => 4,
+      PacketData::LeaveTtyModeRequest => 0,
+    }
+  }
+}
 #[binrw]
 #[brw(big)]
+#[br(assert(size as usize == data.size()))]
 #[derive(Debug, PartialEq, Eq, Clone)]
 struct Packet {
+  #[br(temp)]
+  #[bw(calc(data.size() as u32))]
   size: u32,
   ty: PacketType,
   #[br(args(size, ty))]
-  #[bw(args(*size, *ty))]
   data: PacketData,
 }
